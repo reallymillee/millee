f87be3db9946d619979ef83f1b2410e5f18dcada
Millee <7035205+reallymillee@users.noreply.github.com>
2018-05-28 11:43:37 +1000

    ahdfhadga

--- projects/mosaic-game/script.js
+++ projects/mosaic-game/script.js
@@ -7,9 +7,9 @@ window.onload = function randomNumbers() {
       rand = Math.floor(Math.random() * 25 + 1);
     } while (jQuery.inArray(rand, numbers) !== -1);
     numbers.push(rand);
-    var value = "#num" + i;
-    console.log(value);
-    $(value).attr('src') = "projects/mosaic-game/" + rand + ".jpg";
+    var value = "num" + i;
+    console.log(value)
+    document.getElementById(value).src = "projects/mosaic-game/" + rand + ".jpg";
   }
   check();
 };
